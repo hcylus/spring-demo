d1f0e8e7c8e0478e51e4cca8c028fea1ca2c678b
hcylus <hercylus@gmail.com>
2020-05-25 23:36:31 +0800

    add application.properties content

--- src/main/java/com/devops/demo/InfoController.java
+++ src/main/java/com/devops/demo/InfoController.java
@@ -1,5 +1,6 @@
 package com.devops.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,10 +12,17 @@ import java.util.*;
 @Controller
 public class InfoController {
 
-    @RequestMapping("/info")
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    @RequestMapping("/show")
     public String showInfo(Model model) {
-        model.addAttribute("info", SystemUtil.SystemInfo());
-        return "info";
+        HashMap<String, String> map = new HashMap<>();
+        map.put("AppName",applicationName);
+        HashMap<String, String> info = SystemUtil.SystemInfo();
+        info.putAll(map);
+        model.addAttribute("show", info);
+        return "show";
     }
 
 }
